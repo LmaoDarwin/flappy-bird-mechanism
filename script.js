fc7fe7f4c03a1c4a5aa482a4e36bee8c7dfce52e
darwin <d.f.winzi2006@gmail.com>
2023-04-26 18:24:10 +0700

    refactor

--- script.js
+++ script.js
@@ -12,50 +12,43 @@ class Game {
     this.ufo = new UFO(this);
   }
   /**
-   * Canvas render context 2d
+   * to constantly draw on canvas
    * @param {CanvasRenderingContext2D} ctx
    */
   draw(ctx) {
     this.ufo.draw(ctx);
   }
-  /** Any update without drawing context goes below*/
+  /** Any update without drawing context goes below */
   update() {
     this.ufo.controller();
     this.ufo.outOfBound();
   }
 }
 class UFO {
-  /**
-   * @param {Game} game
-   */
+  /** @param {Game} game */
   constructor(game) {
     this.game = game;
     this.x = this.game.width / 2 - this.size / 2; //start at middle
     this.y = this.game.height / 2; //start at middle
     window.addEventListener('keydown', ({ key }) => {
-      if (key === ' ') {
-        if (this.state === 'fly') return; //prevent double click
-        (this.state = 'fly'), (this.speed = -10); //fly up by changing speed
-      }
+      if (this.state === 'fly') return; //prevent double click
+      if (key === ' ') (this.state = 'fly'), (this.speed = -10); //fly up by changing speed
     });
-    window.addEventListener('keyup', ({ key }) => key === ' ' && (this.state = '')); //space postclick set state back to ''
+    window.addEventListener('keyup', ({ key }) => key === ' ' && (this.state = '')); //reset state back to '' after space keyup
   }
 
   //    --- PROPERTIES ---
   size = 20; //size of cube
-  state = ''; //state => 'fly' || ''
+  state = ''; //state = 'fly' || ''
   speed = 0; //speed of cube
 
   //    --- METHODS ---
-  /**
-   * Canvas render context 2d to draw stuff on canvas
-   * @param {CanvasRenderingContext2D} ctx
-   */
+  /** @param {CanvasRenderingContext2D} ctx */
   draw(ctx) {
     ctx.fillStyle = 'orangered';
     ctx.fillRect(this.x, this.y, this.size, this.size);
   }
-  /** to place cube according to the edge of the canvas */
+  /** to prevent cube going beyond the canvas */
   outOfBound() {
     if (this.y < 0) (this.y = 0), (this.speed = 0); //ocollide with above
     if (this.y >= this.game.height - this.size) (this.y = this.game.height - this.size), (this.speed = 0); // oncollide with bottom
@@ -67,7 +60,6 @@ class UFO {
   }
 }
 const myGame = new Game(cvs.height, cvs.width); //init game class
-
 /** to start animation loop */
 function animate() {
   ctx.clearRect(0, 0, cvs.width, cvs.height);
